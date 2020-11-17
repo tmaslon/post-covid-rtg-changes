88bfd6ced2609223cddc431c872c78b5b7f5034f
Jakub <kubus199712@gmail.com>
2020-11-18 00:30:04 +0100

    saved histogram pics; done histogram energy calculations

--- covidotron/photos_features/histogram_test.py
+++ covidotron/photos_features/histogram_test.py
@@ -9,22 +9,29 @@ path = ['pics/cut_all/healthy', 'pics/cut_all/COVID-19']
 imgList_h = os.listdir(path[0])
 imgList_c = os.listdir(path[1])
 
+health_num = 0
+
 for i in imgList_h:
     imgCurr = cv2.imread(f'{path[0]}/{i}', 0)
     images.append(imgCurr)
+    health_num += 1
 for i in imgList_c:
     imgCurr = cv2.imread(f'{path[1]}/{i}', 0)
     images.append(imgCurr)
 
 imgList_merge = imgList_h + imgList_c
 
-plot = True
+plot = False
+save = False
 
+print("Healthy --------------")
 for i in range(0, len(images)):
-    # podziel na pół histogram i prównaj energie
     img = images[i]
+
     # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     # img = clahe.apply(img)
+    # img = cv2.equalizeHist(img)
+
     h = [(img == v).sum() for v in range(256)]
     h = np.array(h)
     norm_h = h/h.sum()
@@ -32,6 +39,12 @@ for i in range(0, len(images)):
     cumul_h = np.zeros((256,))
     for j in range(256):
         cumul_h[j] = norm_h[j] + cumul_h[j-1]
+
+    if( i == health_num ):
+        print("COVID-19 -------------")
+
+    h2 = cumul_h[255] - cumul_h[126]
+    print(imgList_merge[i], '\t', h2)
 
     if plot is True:
         plt.subplot(121)
@@ -41,5 +54,7 @@ for i in range(0, len(images)):
         plt.bar(range(256), cumul_h)
         # plt.hist(img.ravel(), 256, [0, 256])
         plt.show()
+    if save is True:
+        plt.savefig('pics/cumul_histogram/' + os.path.splitext(imgList_merge[i])[0] + '.png')
 
-
+print("----------------------")
---
+++ covidotron/photos_features/test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+array = [1, 2, 3, 4]
+
+print(sum(array[1:]))
